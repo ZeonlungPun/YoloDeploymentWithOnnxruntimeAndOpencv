aa46b7595db98c56e53923d31f67ec48b0a6b17e
ZeonLungPun <122013127+ZeonlungPun@users.noreply.github.com>
2024-01-24 19:23:02 +0800

    Add files via upload

--- yolov8obbOPENCV/yolov8obb.cpp
+++ yolov8obbOPENCV/yolov8obb.cpp
@@ -2,15 +2,22 @@
 #include <vector>
 #include <opencv2/opencv.hpp>
 #include <math.h>
-
-#define pi acos(-1)
 using namespace std;
 using namespace cv;
 
+// defind some constant variables
+#define pi acos(-1)
 float modelScoreThreshold=0.3;
 float modelNMSThreshold=0.35;
-std::vector<std::string> classes={"person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench","bird"};
+std::vector<std::string> classes{"plane","ship","storage tank","baseball diamond","tennis court" ,"basketball court","ground track field","harbor","bridge","large vehicle","small vehicle","helicopter","roundabout","soccer ball field","swimming pool"};
 
+
+// define a struct to save some information
+typedef struct {
+	cv::RotatedRect box;
+	float score;
+	int Classindex;
+}RotatedBOX;
 
 
 cv::Mat formatToSquare(const cv::Mat &source)
@@ -23,7 +30,7 @@ cv::Mat formatToSquare(const cv::Mat &source)
     return result;
 }
 
-std::vector<cv::RotatedRect> detect_one_image(const cv::Mat &input_image,std::vector<std::string>& classes,cv::dnn::Net& net)
+std::vector<RotatedBOX> detect_one_image(const cv::Mat &input_image,std::vector<std::string>& classes,cv::dnn::Net& net)
 {
     cv::Mat modelInput = input_image;
     modelInput = formatToSquare(modelInput);
@@ -47,9 +54,10 @@ std::vector<cv::RotatedRect> detect_one_image(const cv::Mat &input_image,std::ve
     float x_factor = modelInput.cols / 640.0;
     float y_factor = modelInput.rows / 640.0;
 
-    std::vector<int> class_ids;
-    std::vector<float> confidences;
+   
     std::vector<cv::RotatedRect> boxes;
+    std::vector<RotatedBOX>BOXES;
+    std::vector<float> confidences;
 
     for (int i = 0; i < rows; ++i)
     {
@@ -58,63 +66,71 @@ std::vector<cv::RotatedRect> detect_one_image(const cv::Mat &input_image,std::ve
         cv::Point class_id;
         double maxClassScore;
         minMaxLoc(scores, 0, &maxClassScore, 0, &class_id);
+        RotatedBOX BOX;
         
         if (maxClassScore > modelScoreThreshold)
         {
-                confidences.push_back(maxClassScore);
-                class_ids.push_back(class_id.x);
-              
-                float x = data[0]* x_factor;
-                float y = data[1]* y_factor;
-                float w = data[2]* x_factor;
-                float h = data[3]* y_factor;
+            confidences.push_back(maxClassScore);  
+            float x = data[0]* x_factor;
+            float y = data[1]* y_factor;
+            float w = data[2]* x_factor;
+            float h = data[3]* y_factor;
 
-                float angle=data[dimensions-1];
-                //angle in [-pi/4,3/4 pi) --》 [-pi/2,pi/2)
-                if (angle>=pi && angle <= 0.75*pi)
-                {
-                    angle=angle-pi;
-                }  
-                
-             
+            float angle=data[dimensions-1];
+            //angle in [-pi/4,3/4 pi) --》 [-pi/2,pi/2)
+            if (angle>=pi && angle <= 0.75*pi)
+            {
+                angle=angle-pi;
+            } 
 
-                cv::RotatedRect box=cv::RotatedRect(cv::Point2f(x,y),cv::Size2f(w,h),angle*180/pi);
-                boxes.push_back(box);
+            BOX.Classindex=class_id.x;
+            BOX.score=maxClassScore; 
+            cv::RotatedRect box=cv::RotatedRect(cv::Point2f(x,y),cv::Size2f(w,h),angle*180/pi);
+            BOX.box=box;
+            boxes.push_back(box);
+            BOXES.push_back(BOX);
                 
         }
         data += dimensions;
     }
     cout<<boxes.size()<<endl;
+    //postprocess
     std::vector<int> nms_result;
     cv::dnn::NMSBoxes(boxes, confidences, modelScoreThreshold, modelNMSThreshold, nms_result);
     cout<<nms_result.size()<<endl;
-    std::vector<cv::RotatedRect> Remain_boxes;
+    std::vector<RotatedBOX> Remain_boxes;
     for (unsigned long i = 0; i < nms_result.size(); ++i)
     {
         int idx = nms_result[i];
-        cv::RotatedRect box_=boxes[idx];
-        Remain_boxes.push_back(box_);
+        RotatedBOX Box_=BOXES[idx];
+        Remain_boxes.push_back(Box_);
     }
 
     return Remain_boxes;
     
 }
 
-void Draw(cv::Mat& image,std::vector<cv::RotatedRect>& detect_boxes)
+void Draw(cv::Mat& image,std::vector<RotatedBOX>& detect_boxes)
 {
     for (unsigned long i = 0; i < detect_boxes.size(); ++i)
     {
        
-        cv::RotatedRect box_=detect_boxes[i];
+        RotatedBOX Box_=detect_boxes[i];
+        cv::RotatedRect box_=Box_.box;
         cv::Point2f points[4];
         box_.points(points);
+        int class_id=Box_.Classindex;
+
         // 將旋轉矩形畫到圖像上
         for (int i = 0; i < 4; ++i) 
         {
-            cv::line(image, points[i], points[(i + 1) % 4], cv::Scalar(0, 0, 255), 2);  // 顏色為紅色，線寬為2
+            cv::line(image, points[i], points[(i + 1) % 4], cv::Scalar(0, 0, 255), 2);  
         }
+        cv::putText(image, classes[class_id], points[0], cv::FONT_HERSHEY_PLAIN, 2.0, cv::Scalar(255, 0, 0), 2, 2);
+
 
     }
+
      // 顯示圖像
     cv::imshow("RotatedRect", image);
     cv::waitKey(0);
@@ -125,11 +141,15 @@ void Draw(cv::Mat& image,std::vector<cv::RotatedRect>& detect_boxes)
 
 int main()
 {
-    cv::dnn::Net net = cv::dnn::readNetFromONNX("/home/kingargroo/cpp/yolov8/yolov8n-obb.onnx");
+    cv::dnn::Net net = cv::dnn::readNetFromONNX("/home/punzeonlung/CPP/yolov8-obb/yolov8n-obb.onnx");
     net.setPreferableBackend(cv::dnn::DNN_BACKEND_OPENCV);
     net.setPreferableTarget(cv::dnn::DNN_TARGET_CPU);
-    cv::Mat input_image=cv::imread("/home/kingargroo/cpp/yolov8/test1.jpeg");
-    std::vector<cv::RotatedRect>detect_boxes = detect_one_image(input_image, classes, net);
+    cv::Mat input_image=cv::imread("/home/punzeonlung/CPP/yolov8-obb/test1.jpg");
+    int64 start = cv::getTickCount();
+    std::vector<RotatedBOX>detect_boxes = detect_one_image(input_image, classes, net);
     Draw( input_image, detect_boxes);
+    float t = (cv::getTickCount() - start) / static_cast<float>(cv::getTickFrequency());
+    cv::putText(input_image, cv::format("FPS: %.2f", 1.0 / t), cv::Point(20, 40), cv::FONT_HERSHEY_PLAIN, 2.0, cv::Scalar(255, 0, 0), 2, 8);
+
 
 }
